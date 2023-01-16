c31e5e299bdbb778b3715aafa02f7b002e38574a
MartinGO11 <ogutierrez.m94@gmail.com>
2023-01-16 19:08:32 +0000

    todofetch

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tareas from "./components/tareas";
+import "./App.css";
+
+const App = () => {
+
+  return (
+    <>
+      <Tareas />
+    </>
+  )
+};
+
+export default App
+;
